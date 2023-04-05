b0a3f92f00c718c2edb6154aba202e7e4f976541
kentfx1 <105797352+kentfx1@users.noreply.github.com>
2023-04-04 18:11:10 -0700

    a new change

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Homepage from "./pages/homepage";
 import Me from './pages/Me';
+import Editprofile from './components/Editprofile';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 //import HomeNavbar from './components/HomeNavbar';
 
@@ -15,7 +16,7 @@ function App() {
               <Route path='notification' element= {<Notification/>}/>
               <Route path='message' element= {<Message/>}/> */}
             <Route path='/me' element={<Me/>}/>
-            
+            <Route path='/editprofile' element={<Editprofile/>}/>
           </Routes>
         </BrowserRouter>
     </div>
---
+++ src/components/Editprofile.js
@@ -0,0 +1,86 @@
+import React from "react";
+import Landpics from "../images/rectangle.svg";
+//import './index.css';
+//import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Ppics from '../images/pp.svg';
+import pen from '../icons/Vectorpen.svg';
+import ball from '../icons/ball.svg';
+import links from '../icons/links.svg';
+import linkedin from '../icons/linkedin.svg';
+import HomeNavbar from "../components/HomeNavbar";
+import SidebarMenu from "../components/sidebarmenu/SidebarMenu";
+import Stories from "../components/Stories";
+import Col from "react-bootstrap/Col";
+import HomeSidebar from "../components/HomeSidebar";
+import classes from "./Editprofile.module.css";
+// import { Card, Button } from "react-bootstrap";
+// import cardpics from'./cardpics.svg';
+
+
+
+const Editprofile = () => (
+        <React.Fragment>
+            <div>
+                <HomeNavbar/>
+            </div>
+            <div className="container">
+                <div className="row">
+                    <div class="col-md-1" style={{width: '280px'}}>
+                        <Col md={12}>
+                            <HomeSidebar />
+                  
+                            <Stories>
+                                <SidebarMenu />
+                            </Stories>
+                        </Col>
+                    </div>
+                    <div className={`${classes["col-md-5"]}`} style={{width: '560px'}}>
+                        <h6>User Profile</h6>
+                        <div className={`${classes["card"]}`}>
+                            <img src={Landpics} className={`${classes["card-img-top"]}`} alt="pics"/>
+                            <img src={pen} alt="pen" className={`${classes["pen2"]}`}/>
+                            <fieldset>
+                                <img src={Ppics} className={`${classes["card-top"]}`} alt="pics" style={{width: '100px'}}/> <img src={pen} alt="pen" className={`${classes["pen"]}`}/>
+                                <div className={`${classes["card-body"]}`}>
+                                    <form action="#" className={`${classes["form"]}`} >
+                                        <label htmlFor="display-name">Display Name</label><span className={`${classes["span"]}`}>Edit</span><br/>
+                                        <input type="text" placeholder="Enter your Name"  className={`${classes["input"]}`}/><br/><br/>
+                                        <label htmlFor="bio">Bio</label><span className={`${classes["span"]}`}>Edit</span><br/>
+                                        <input type="text" placeholder="Describe yourself in few words" className={`${classes["input"]}`} /><br/><br/>
+                                        <label htmlFor="location">Location</label><span className={`${classes["span"]}`}>Edit</span><br/>
+                                        <input type="text" placeholder="Where's your location" className={`${classes["input"]}`}/>
+                                    </form><br/>
+                                    <div className={`${classes["hobbies"]}`}>
+                                        <p>Hobbies & Interests<span className={`${classes["span"]}`}>Add</span></p>
+                                        <div className={`${classes["list"]}`}>
+                                            <p>Dancing</p>
+                                            <p>Swimming</p>
+                                            <p>Talking</p>
+                                            <p>Sports</p>
+                                            <p>Crypto</p>
+                                            <p>Programming</p>
+                                        </div><br/>
+                                        <div className={`${classes["social"]}`}>
+                                            <p>Social Links</p><span className={`${classes["span-s"]}`}>Add</span><br/>
+                                            <img src={ball} alt="ball" />
+                                            <img src={links} alt="links" />
+                                            <img src={linkedin} alt="linkedin" />
+                                        </div>
+                                    </div>
+                                    <button className={`${classes["bttn"]}`}>Proceed</button>
+                                </div>
+                            </fieldset>
+                            
+                            
+                        </div>
+                    </div>
+                    <div>
+
+                    </div>
+                </div>
+            </div>
+        </React.Fragment>
+     );
+
+ 
+export default Editprofile;
